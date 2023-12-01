6a5569de181ba6933e4369e0cfa545f3ae63bd2f
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-12-01 13:41:38 +0100

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,51 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";;
 
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import ViteFonts from 'unplugin-fonts/vite'
+
+// import { fileURLToPath, URL } from 'node:url'
+
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [
+    vue(
+      // {template: { transformAssetUrls }}
+    ),
+    
+    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+    vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss',
+      },
+    }),
+
+    ViteFonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900',
+        }],
+      },
+    }),
+
+  ],
+
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   },
+  //   extensions: [
+  //     '.js',
+  //     '.json',
+  //     '.jsx',
+  //     '.mjs',
+  //     '.ts',
+  //     '.tsx',
+  //     '.vue',
+  //   ],
+  // },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
