80d0ff66006f57524f9afc792ce94a7643c3f238
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-12-05 10:46:30 +0100

    Slight refactor

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -15,46 +15,72 @@ struct Item {
     children: Option<Vec<Item>>,
 }
 
-#[derive(Default)]
 struct Node {
     id: Option<u32>,
     children: HashMap<String, Node>,
 }
 
-fn paths_to_items(paths: Vec<PathBuf>) -> Vec<Item> {
-    let mut root = Node::default();
-    let mut id_counter = 1;
-
-    for path in paths {
-        let mut node = &mut root;
-        for component in path.components() {
-            let key = component.as_os_str().to_string_lossy().into_owned();
-            node = node.children.entry(key).or_insert_with(Node::default);
+impl Default for Node {
+    fn default() -> Self {
+        Node {
+            id: None,
+            children: HashMap::new(),
         }
-        node.id = Some(id_counter);
-        id_counter += 1;
+    }
+}
+
+fn paths_to_file_tree(paths: Vec<PathBuf>) -> Vec<Item> {
+
+
+    /// This helper function builds a tree structure in `root` from the given `paths`.
+    ///
+    /// Each `PathBuf` in `paths` is split into components, and for each component,
+    /// a new `Node` is created if it doesn't already exist. The `Node` is then inserted
+    /// as a child of the current node in the tree. The last node in each path is assigned
+    /// a unique ID.
+    ///
+    /// # Arguments
+    ///
+    /// * `paths` - A vector of `PathBuf` representing the paths to be added to the tree.
+    /// * `root` - The root `Node` of the tree.
+    /// * `id_counter` - A counter used to assign unique IDs to the nodes.
+    fn build_tree(paths: Vec<PathBuf>, root: &mut Node, id_counter: &mut u32) {
+        for path in paths {
+            let mut node = root;
+            for component in path.components() {
+                let key = component.as_os_str().to_string_lossy().into_owned();
+                node = node.children.entry(key).or_insert_with(Node::default);
+            }
+            node.id = Some(*id_counter);
+            *id_counter += 1;
+        }
     }
 
-    fn convert(node: Node, name: String) -> Item {
+    fn convert_node_into_item(node: Node, name: String) -> Item {
         Item {
             id: node.id.unwrap(),
             name,
             children: if node.children.is_empty() {
                 None
             } else {
-                Some(node.children.into_iter().map(|(name, node)| convert(node, name)).collect())
+                Some(node.children.into_iter().map(|(name, node)| convert_node_into_item(node, name)).collect())
             },
         }
     }
 
-    root.children.into_iter().map(|(name, node)| convert(node, name)).collect()
+    let mut root = Node::default();
+    let mut id_counter = 1;
+
+    build_tree(paths, &mut root, &mut id_counter);
+    root.children.into_iter().map(|(name, node)| convert_node_into_item(node, name)).collect()
+
 }
 
 #[command]
 fn get_data() -> Vec<Item> {
     let default_path = "/Users/kasper_janehag/Library/CloudStorage/OneDrive-McKinsey&Company/Documents/repos/dejavy/tests";
     let paths = list_paths(default_path, true);
-    let file_tree = paths_to_items(paths);
+    let file_tree = paths_to_file_tree(paths);
     file_tree
 }
 
