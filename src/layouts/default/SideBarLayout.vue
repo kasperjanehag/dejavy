f57ea2ff180587a0550f1f28de2a1ef98da51709
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-12-05 23:14:28 +0100

    Adding file picker

--- src/layouts/default/SideBarLayout.vue
+++ src/layouts/default/SideBarLayout.vue
@@ -1,8 +1,23 @@
 <template>
   <v-navigation-drawer width="450">
 
-    <v-list-item title="Image Formats"></v-list-item>
-    <v-divider></v-divider>
+    
+    
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+          <v-card class="d-flex align-center" flat tile>
+            <v-card-actions class="flex-grow-1 rounded-l">
+              <v-btn color="primary" @click="selectDirectory" block rounded="xl" class="subtitle-1">Select Directory</v-btn>
+            </v-card-actions>
+            <v-card-text class="flex-grow-1 rounded-r d-flex align-center justify-center subtitle-1">
+              <span>{{ displayedDirectory }}</span>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    
     <v-container fluid>
       <v-row justify="center" align="center">
         <v-col cols="auto">
@@ -27,42 +42,29 @@
         </v-col>
       </v-row>
     </v-container>
-
-
-    <v-list-item title="Image Explorer"></v-list-item>
     
-    <v-container>
-      <v-btn color="primary" @click="selectDirectory">Select Directory</v-btn>
-      
-      <v-dialog v-model="dialog" persistent max-width="600px">
-        <v-card>
-          <v-card-title>
-            Selected Directory
-          </v-card-title>
-          <v-card-text>
-            {{ selectedDirectory }}
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-container>
-
     <v-divider></v-divider>
     <nested-list/>
   </v-navigation-drawer>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { open } from '@tauri-apps/api/dialog';
 
 const extensions = ['png', 'jpeg', 'jpg', 'gif', 'bmp', 'tiff', 'ico', 'webp'];
 const activeExtensions = ref<string[]>(extensions);
-const dialog = ref(false);
 const selectedDirectory = ref('');
+
+const displayedDirectory = computed(() => {
+  if (selectedDirectory.value) {
+    let parts = selectedDirectory.value.split('/');
+    let lastThreeParts = parts.slice(-3).join('/');
+    return lastThreeParts.length > 20 ? '...' + lastThreeParts.slice(-20) : '...' + lastThreeParts;
+  } else {
+    return "No path selected ...";
+  }
+});
 
 const toggleExtension = (ext: string) => {
   const index = activeExtensions.value.indexOf(ext);
@@ -78,7 +80,6 @@ const selectDirectory = async () => {
     const selected = await open({ directory: true, multiple: false });
     if (selected) {
       selectedDirectory.value = Array.isArray(selected) ? selected[0] : selected;
-      dialog.value = true;
     }
   } catch (error) {
     console.error('Error selecting directory:', error);
@@ -101,4 +102,6 @@ export default {
 .equal-width {
   width: 80px; /* Adjust this value to your needs */
 }
+
+
 </style>
