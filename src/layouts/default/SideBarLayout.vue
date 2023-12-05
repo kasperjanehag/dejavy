166a5f121ed7f6d4da11529859b2bbfa39abafa4
Kasper Janehag <kasper_janehag@mckinsey.com>
2023-12-05 15:14:12 +0100

    Adding clickable select directory button

--- src/layouts/default/SideBarLayout.vue
+++ src/layouts/default/SideBarLayout.vue
@@ -27,7 +27,29 @@
         </v-col>
       </v-row>
     </v-container>
+
+
     <v-list-item title="Image Explorer"></v-list-item>
+    
+    <v-container>
+      <v-btn color="primary" @click="selectDirectory">Select Directory</v-btn>
+      
+      <v-dialog v-model="dialog" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            Selected Directory
+          </v-card-title>
+          <v-card-text>
+            {{ selectedDirectory }}
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="dialog = false">Close</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-container>
+
     <v-divider></v-divider>
     <nested-list/>
   </v-navigation-drawer>
@@ -35,10 +57,12 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import NestedList from '../../components/NestedList.vue';
+import { open } from '@tauri-apps/api/dialog';
 
 const extensions = ['png', 'jpeg', 'jpg', 'gif', 'bmp', 'tiff', 'ico', 'webp'];
 const activeExtensions = ref<string[]>(extensions);
+const dialog = ref(false);
+const selectedDirectory = ref('');
 
 const toggleExtension = (ext: string) => {
   const index = activeExtensions.value.indexOf(ext);
@@ -48,6 +72,19 @@ const toggleExtension = (ext: string) => {
     activeExtensions.value.push(ext);
   }
 };
+
+const selectDirectory = async () => {
+  try {
+    const selected = await open({ directory: true, multiple: false });
+    if (selected) {
+      selectedDirectory.value = Array.isArray(selected) ? selected[0] : selected;
+      dialog.value = true;
+    }
+  } catch (error) {
+    console.error('Error selecting directory:', error);
+  }
+};
+
 </script>
 
 <script lang="ts">
